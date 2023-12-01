386fb34794a3e74fade06c1f513cacade58db00c
Elvira Zakharova <elvizaharova2004@gmail.com>
2023-12-01 10:53:02 +0300

    second

--- scr.py
+++ scr.py
@@ -1,63 +1,55 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from database import Database
 import sqlite3
 
 
-def create_database():
-    connection = sqlite3.connect('product_parf.db')
-    cur = connection.cursor()
-
-    cur.execute('''CREATE TABLE IF NOT EXISTS product                 
-                   (title TEXT, price TEXT, img TEXT)''')
-
-    connection.commit()
-    connection.close()
+class Search:
+    def __init__(self, query, offset):
+        self.query = query
+        self.offset = offset
+        self.driver = webdriver.Chrome()
 
 
-def get_product_by_query(query, offset):
-    driver = webdriver.Chrome()
-    driver.get(f'https://zielinskiandrozen.ru/magazin/search/?keyword={query}&offset={offset}')
-    driver.implicitly_wait(10)
-    html = driver.execute_script("return document.body.innerHTML")
-    driver.close()
+    def search(self):
+        self.driver.get(
+            f'https://zielinskiandrozen.ru/magazin/search/?keyword={self.query}&offset={self.offset}')
+        self.driver.implicitly_wait(10)
+        html = self.driver.execute_script("return document.body.innerHTML")
+        self.driver.close()
 
-    bsObj = BeautifulSoup(html, 'html.parser')
-    found_goods = bsObj.find_all('div', {'class': "grid-product__wrap-inner"})
-    all_goods = []
+        bsObj = BeautifulSoup(html, 'html.parser')
+        found_goods = bsObj.find_all('div', {'class': "grid-product__wrap-inner"})
+        all_goods = []
 
-    for i in found_goods:
-        image = i.find('div', {"class": 'grid-product__image-wrap'})
-        img = image.find('img').get('src')
-        title = i.find('div', {"class": 'grid-product__title-inner'}).text.strip()
-        price = i.find('div', {"class": "grid-product__price-value ec-price-item"}).text.replace("\xa0", '')
+        for i in found_goods:
+            image = i.find('div', {"class": 'grid-product__image-wrap'})
+            img = image.find('img').get('src')
+            title = i.find('div', {"class": 'grid-product__title-inner'}).text.strip()
+            price = i.find('div', {"class": "grid-product__price-value ec-price-item"}).text.replace("\xa0", '')
 
-        all_goods.append((title, price, img))
+            all_goods.append((title, price, img))
 
-    return all_goods
-
-
-def insert_into_database(data):
-    connection = sqlite3.connect('product_parf.db')
-    c = connection.cursor()
-    c.executemany('INSERT INTO product VALUES (?, ?, ?)', data)
-
-    connection.commit()
-    connection.close()
+        return all_goods
 
 
-def get_all_products_by_query(query):
+def get_all_products(query):
     all_goods = []
     offset = 0
     while True:
-        goods = get_product_by_query(query, offset)
+        scraper = Search(query, offset)
+        goods = scraper.search()
         all_goods.extend(goods)
         if len(goods) < 60:
             break
         offset += 60
 
-    create_database()
-    insert_into_database(all_goods)
+    db = Database()
+    db.create_table_parf()
+    db.insert_data_parf(all_goods)
+    db.close_connection()
+
     return all_goods
 
-get_all_products_by_query('мыльница')
+get_all_products('соль')
 
